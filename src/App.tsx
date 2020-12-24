c7cb5224c7bc5c1bb2d821d8a16228266f367c6a
Arciiix <artur.nowak100@gmail.com>
2020-12-24 23:45:56 +0100

    Create Home component (not finished yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <span>hello world!</span>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./Home.module.css";
+
+class Home extends React.Component {
+  render() {
+    return <div className={styles.container}>test</div>;
+  }
+}
+
+export default Home;
